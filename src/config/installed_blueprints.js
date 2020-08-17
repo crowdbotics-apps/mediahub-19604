4e107f291ab0f5e3e582dabab60f4556e7c69d64
Crowdbotics <team@crowdbotics.com>
2020-08-17 03:47:17 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings91019', human_name: 'Settings', access_route: 'Settings91019'},
 { name: 'Settings91011', human_name: 'Settings', access_route: 'Settings91011'},
 { name: 'UserProfile91009', human_name: 'User Profile', access_route: 'UserProfile91009'},
 
